b3adfb0443557fefe775ba5e089f5fbb60593978
kdohyeon <kdohyeon@gmail.com>
2023-12-30 17:19:52 +0900

    a, b Squashed commit of the following:
    commit 211fa939f47604a2b37325be50652655e2a7f5c2
    Author: kdohyeon <kdohyeon@gmail.com>
    Date:   Sat Dec 30 17:19:05 2023 +0900
    
        line delete B
    
    commit 4c2dd3927c8323b156a33f0206035a147e387a21
    Author: kdohyeon <kdohyeon@gmail.com>
    Date:   Sat Dec 30 17:18:48 2023 +0900
    
        line delete A

--- src/main/java/com/example/gitstudy2/A.java
+++ src/main/java/com/example/gitstudy2/A.java
@@ -3,8 +3,5 @@ package com.example.gitstudy2;
 public class A {
     public void test() {
 
-
-
-
     }
 }
--- src/main/java/com/example/gitstudy2/B.java
+++ src/main/java/com/example/gitstudy2/B.java
@@ -3,7 +3,5 @@ package com.example.gitstudy2;
 public class B {
     public void test() {
 
-
-
     }
 }
