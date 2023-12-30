55ea803a1a03f8ef67b29e9641e1fd7a6f5e7259
kdohyeon <kdohyeon@gmail.com>
2023-12-30 14:23:12 +0900

    fff

--- src/main/java/com/example/gitstudy2/GitStudy2Application.java
+++ src/main/java/com/example/gitstudy2/GitStudy2Application.java
@@ -12,6 +12,7 @@ public class GitStudy2Application {
         System.out.println("a");
         System.out.println("b");
         System.out.println("c");
+        System.out.println("d");
         SpringApplication.run(GitStudy2Application.class, args);
     }
 
